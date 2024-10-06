151c322ff10224956a70002e25cbd3fb96c7511b
luiz.junior <luiz.junior@neomind.com.br>
2024-10-06 01:53:33 -0300

    Editando, excluindo e criando uma dashboard para um user

--- src/api/controllers/HouseController.js
+++ src/api/controllers/HouseController.js
@@ -1,6 +1,14 @@
+import { restart } from "nodemon";
 import House from "../../domain/models/House";
+import User from "../../domain/models/User";
 
 class HouseController {
+
+    async index(req,res){
+        const { status } = req.query;
+        const houses = await House.find({status});
+        return res.json(houses);
+    }
 
     async store(req,res) {
 
@@ -21,6 +29,48 @@ class HouseController {
 
         return res.json(house);
     };
+
+    async update(req,res) {
+        const { id }= req.params;
+        const { filename } = req.file;
+        const { description, price, location, status } = req.body;
+        const { user_id } = req.headers;
+
+        
+        const user = await User.findById(user_id);
+        const findHouse = await House.findById(id);
+
+        if(String(user._id) !== String(findHouse.user)){
+            return res.status(401).json({error: 'Não autorizado'});
+        }
+
+        await House.updateOne({_id: id}, {
+            user: user_id,
+            thumbnail: filename,
+            description,
+            price,
+            location,
+            status,
+        });
+
+        return res.send();
+    }
+
+    async destroy(req, res){
+        const { id } = req.body;
+        const { user_id } = req.headers;
+
+        const user = await User.findById(user_id);
+        const findHouse = await House.findById(id);
+
+        if(String(user._id) !== String(findHouse.user)){
+            return res.status(401).json({error: 'Não autorizado'});
+        }
+
+        await House.findByIdAndDelete({_id: id});
+        
+        return res.json({message: 'Deletado com sucesso'});
+    }
 }
 
 export default new HouseController();
--- src/route.js
+++ src/route.js
@@ -1,8 +1,11 @@
 import Router from 'express';
-import SessionController from './api/controllers/SessionController'; muklt
-import HouseController from './api/controllers/HouseController';
 import multer from 'multer';
 import uploadConfig from './config/upload';
+import House from './domain/models/House';
+
+import SessionController from './api/controllers/SessionController';
+import HouseController from './api/controllers/HouseController';
+import DashBoardController from './api/controllers/DashBoardController';
 
 const routes = new Router();
 const upload = multer(uploadConfig);
@@ -10,5 +13,11 @@ const upload = multer(uploadConfig);
 routes.post('/sessions', SessionController.store);
 
 routes.post('/houses', upload.single('thumbnail'), HouseController.store);
+routes.get('/houses', HouseController.index);
+routes.put('/houses/:id', upload.single('thumbnail'), HouseController.update);
+routes.delete('/houses', HouseController.destroy);
+
+routes.get('/dashboard', DashBoardController.show);
+
 
 export default routes;
